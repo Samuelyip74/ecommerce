fceec58fbedb9a3ee31bc713cda84ad1c487941c
Samuelyip74 <samuel.yip@live.com>
2019-07-02 23:14:14 +0800

    first

--- core/models.py
+++ core/models.py
@@ -1,6 +1,9 @@
+from django.db.models.signals import post_save
 from django.conf import settings
-
 from django.db import models
+from django.db.models import Sum
+from django.shortcuts import reverse
+from django_countries.fields import CountryField
 
 CATEGORY_CHOICES = (
     ('S', 'Shirt'),
@@ -81,14 +84,14 @@ class Order(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
-    #shipping_address = models.ForeignKey(
-    #    'Address', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
-    #billing_address = models.ForeignKey(
-    #    'Address', related_name='billing_address', on_delete=models.SET_NULL, blank=True, null=True)
-    #payment = models.ForeignKey(
-    #    'Payment', on_delete=models.SET_NULL, blank=True, null=True)
-    #coupon = models.ForeignKey(
-    #    'Coupon', on_delete=models.SET_NULL, blank=True, null=True)
+    shipping_address = models.ForeignKey(
+        'Address', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
+    billing_address = models.ForeignKey(
+        'Address', related_name='billing_address', on_delete=models.SET_NULL, blank=True, null=True)
+    payment = models.ForeignKey(
+        'Payment', on_delete=models.SET_NULL, blank=True, null=True)
+    coupon = models.ForeignKey(
+        'Coupon', on_delete=models.SET_NULL, blank=True, null=True)
     being_delivered = models.BooleanField(default=False)
     received = models.BooleanField(default=False)
     refund_requested = models.BooleanField(default=False)
@@ -115,3 +118,56 @@ class Order(models.Model):
         if self.coupon:
             total -= self.coupon.amount
         return total
+
+class Address(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    country = CountryField(multiple=False)
+    zip = models.CharField(max_length=100)
+    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+    default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
+
+
+class Payment(models.Model):
+    stripe_charge_id = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.SET_NULL, blank=True, null=True)
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=15)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.code
+
+
+class Refund(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    reason = models.TextField()
+    accepted = models.BooleanField(default=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.pk}"
+
+
+def userprofile_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        userprofile = UserProfile.objects.create(user=instance)
+
+
+post_save.connect(userprofile_receiver, sender=settings.AUTH_USER_MODEL)
